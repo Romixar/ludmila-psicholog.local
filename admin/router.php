0961d6fff3e5abb7c9df22c9398427117fa84ec6
romixar <you@example.com>
2017-02-20 13:52:10 +0300

    убрал лишнее в маршрутизаторе

--- admin/router.php
+++ admin/router.php
@@ -14,28 +14,21 @@ class Router{
         
 		if(!empty($ctrl) && preg_match($reg,$ctrl)){// парсить URI и вставить ЧПУ красивый адрес
             
-            debug($url);
-            
             $arr = []; // будет массив из имени КОНТРОЛЛЕРА и его МЕТОДА
-            //$ctrl = substr($url,6);// беру после слова /admin
             
 			foreach(Config::$routes as $key => $val){// перебор массива маршрутов
                 
 				if($ctrl == $key) $arr = explode('/',$val[0]);
-			}
-            
-            $ctrl_name = $ctrl.'controller';
-            if($this->checkClassName($ctrl_name)) $method = 'action'.ucfirst($arr[1]);
-            else{
-                $ctrl_name = 'maincontroller';// запуск контроллера по умолчанию
-			    $method = 'actionAll';
-            }
-		}else{
-            
-			$ctrl_name = 'maincontroller';// запуск контроллера по умолчанию
-			$method = 'actionAll';
-		}
 
+			}
+            if(!empty($arr)){
+                $ctrl_name = $ctrl.'controller';
+                $method = 'action'.ucfirst($arr[1]);
+                return;
+            }
+		}         
+		$ctrl_name = 'maincontroller';// запуск контроллера по умолчанию
+		$method = 'actionAll';
 	}
     
 	
@@ -47,19 +40,17 @@ class Router{
 		$view = new View();
 	    $view -> display('head');
         
+        echo 'контроллер - '.$ctrl_name.'<br/>';
+		echo 'метод - '.$method.'<br/>';
+        
 		$cont = new $ctrl_name();// запуск выбранного контроллера и его метода
 		$cont -> $method();
 				
 		
-		echo 'контроллер - '.$ctrl_name.'<br/>';
-		echo 'метод - '.$method.'<br/>';
+		
 		
 	}
 	
-	private function checkClassName($cl){// проверка есть ли такой класс
-        echo 'попал'.$cl;
-        return class_exists($cl);
-    }
 	
 	
 	
