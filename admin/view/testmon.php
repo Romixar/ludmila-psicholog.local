bf0a6c3d7069d57bcefbd6e47d948b00e2f5c02e
romixar <you@example.com>
2017-02-24 10:29:34 +0300

    настраиваю вывод первой стр после успеш авториз

--- admin/view/testmon.php
+++ admin/view/testmon.php
@@ -16,28 +16,28 @@
 				
                     if(!is_array($this->data)) return false;
                     else{
-                        for($i=0; $i<count($this->data); $i++){      
+                        for($i=0; $i<count($this->data); $i++){
                 ?>
                     <tr>
                         <td>
-                           <input type="hidden" name="id_<?= $i ?>" value="<?= $this->data[$i]['id'] ?>" />
-                           <input type="checkbox" <?php if($this->data[$i]['view']==1){ ?>checked<?php } ?> name="view_<?= $i ?>" value="<?= $this->data[$i]['view'] ?>">
+                           <input type="hidden" name="id_<?= $i ?>" value="<?= $this->data[$i]->id ?>" />
+                           <input type="checkbox" <?php if($this->data[$i]->view==1){ ?>checked<?php } ?> name="view_<?= $i ?>" value="<?= $this->data[$i]->view ?>">
                         
                         </td>
                         <td>
-                            <input class="inptitledesc<?php if($this->data[$i]['name-err']){?> error<?php } ?>" type="text" name="name_<?= $i ?>" value="<?= $this->data[$i]['name'] ?>" />
+                            <input class="inptitledesc<?php if(!empty($this->data[$i]->name_err)){?> error<?php } ?>" type="text" name="name_<?= $i ?>" value="<?= $this->data[$i]->name ?>" />
                             <p>Дата добавления: (дд.мм.гггг)</p>
-                            <input class="<?php if($this->data[$i]['dateadd-err']){?> error<?php } ?>" type="date" name="dateadd_<?= $i ?>" value="<?= strftime('%Y-%m-%d',$this->data[$i]['dateadd']) ?>" />
+                            <input class="<?php if(!empty($this->data[$i]->dateadd_err)){?> error<?php } ?>" type="date" name="dateadd_<?= $i ?>" value="<?= strftime('%Y-%m-%d',$this->data[$i]->dateadd) ?>" />
                         
                         </td>
                         <td>
                             
-                            <textarea class="prewtestm<?php if($this->data[$i]['head-err']){?> error<?php } ?>" name="head_<?= $i ?>" cols="50" rows="3"><?= $this->data[$i]['head'] ?></textarea>
+                            <textarea class="prewtestm<?php if(!empty($this->data[$i]->head_err)){?> error<?php } ?>" name="head_<?= $i ?>" cols="50" rows="3"><?= $this->data[$i]->head ?></textarea>
                             <br/>
-                            <textarea class="inpdesc<?php if($this->data[$i]['body-err']){?> error<?php } ?>" name="body_<?= $i ?>" cols="50" rows="8"><?= $this->data[$i]['body'] ?></textarea>
+                            <textarea class="inpdesc<?php if(!empty($this->data[$i]->body_err)){?> error<?php } ?>" name="body_<?= $i ?>" cols="50" rows="8"><?= $this->data[$i]->body ?></textarea>
                         </td>
                         <td>
-                            <a href="<?= Config::HOST_ADDRESS ?>?ctrl=1&id=<?= $this->func ?>_<?= $this->data[$i]['id'] ?>">Удалить</a>
+                            <a href="<?= Config::HOST_ADDRESS ?>?ctrl=1&id=<?= $this->func ?>_<?= $this->data[$i]->id ?>">Удалить</a>
                         </td>
                     </tr>
                     <tr>
